3c17c202d958ffd517ffd5aec2c75170ca6fdc53
Patrick Pigmans <ppigmans@hotmail.com>
2016-02-23 15:04:03 +0100

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -1,4 +1,4 @@
-wget https://apt.puppetlabs.com/puppetlabs-release-pc1-wily.deb
+wget http://apt.puppetlabs.com/puppetlabs-release-pc1-wily.deb
 sudo dpkg -i puppetlabs-release-pc1-wily.deb
 sudo apt-get update
 sudo apt-get install puppet -y
